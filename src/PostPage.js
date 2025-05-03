e2dadfb7e00c5225bcf76f4a9712c884f9854ab3
Eva Ntourou <evedour@gmail.com>
2025-05-03 17:14:42 +0300

    Tutorial 16 & 17

---
+++ src/PostPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import Post from './Post';
+
+const PostPage = ({posts, handleDelete}) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
+  return (
+    <main className='PostPage'>
+        <article className='post'>
+          {post &&
+            <>
+              <h2>{post.title}</h2>
+              <p className='postDate'>{post.datetime}</p>
+              <p className='postBody'>{post.body}</p>
+              <button onClick={() => handleDelete(post.id)}> Delete post </button>
+            </>
+          }
+          {!post &&
+            <>
+              <h2>Post not found</h2>
+              <p>Well, that's dissapointing</p>
+              <p>
+                <Link to='/'>Go home</Link>
+              </p>
+            </>
+          }
+        </article>
+    </main>
+  )
+}
+
+export default PostPage
